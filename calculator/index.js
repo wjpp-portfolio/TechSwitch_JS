963306326c93d2d3af77e17f5c2eaa5a1135dc50
Will <will@wiri.co.uk>
2023-06-20 11:42:56 +0100

    first exercise

--- calculator/index.js
+++ calculator/index.js
@@ -1,1 +1,11 @@
 console.log('Welcome to the calculator!');
+const readline = require('readline-sync');
+
+console.log('Please enter first operand');
+const op1 = readline.prompt();
+
+console.log('Please enter second operand');
+const op2 = readline.prompt();
+
+var answer = parseInt(op1) * parseInt(op2);
+console.log(answer);
