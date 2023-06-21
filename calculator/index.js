0aa4b4b869287eeac16ffd29718157e70f17a907
Will <will@wiri.co.uk>
2023-06-21 12:31:56 +0100

    added vowel counting to calculator

--- calculator/index.js
+++ calculator/index.js
@@ -32,13 +32,55 @@ function Calculate(expression) {
     }
     return answer;
 }
+function IsVowel(prompt) {
+    if ('AEIOU'.indexOf(prompt) == -1) {
+        return false; 
+    } else{
+        return true;
+    }
+}
+class VowelCount {
+    constructor(prompt) {
+        this.prompt = prompt.toUpperCase();
+    }
+    show() {
+        PrintMessage('The vowel counts (as they appear) are:');
+        for (var vowel in this.vowel_map) {
+            let v = this.vowel_map[vowel];
+            PrintMessage(`${vowel}: ${v}:`);
+        }
+    }
+    count() {
+        this.vowel_map = {};
+        for (var i in this.prompt) {
+            var letter = this.prompt[i];
+
+            if (IsVowel(letter)) {
+                if (typeof(this.vowel_map[letter]) === 'undefined') {
+                    this.vowel_map[letter] = 1;
+                } else {
+                    this.vowel_map[letter] += 1;
+                } 
+            } 
+        }
+    }
+
+}
 
 PrintMessage('Welcome to the calculator!');
 
 while (true){
+    var mode = GatherInput('Which calculation mode do you want? \n1) Arithmetic \n2) Vowel counting');
 
-    const operator = GatherInput('Please enter desired operator. (Enter q to quit)');
-    const operands = GatherInput('Please enter space-separated integers');
+    if (mode == 1) {
+        const operator = GatherInput('Please enter desired operator. (Enter q to quit)');
+        const operands = GatherInput('Please enter space-separated integers');
 
-    PrintMessage(Calculate(ParseInputString(operands, operator))); 
+        PrintMessage(Calculate(ParseInputString(operands, operator))); 
+    } else {
+        const string_to_count = GatherInput('Please enter a phrase:');
+        vc = new VowelCount(string_to_count);
+        vc.count();
+        vc.show();
+    }
 }
