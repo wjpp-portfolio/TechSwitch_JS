5c9f532255b2ea32d72906f4ce5b89ca873be927
Will <will@wiri.co.uk>
2023-06-21 08:08:32 +0100

    inluded switch statement

--- calculator/index.js
+++ calculator/index.js
@@ -1,11 +1,30 @@
 console.log('Welcome to the calculator!');
 const readline = require('readline-sync');
 
+console.log('Please enter desired operator');
+const operator = readline.prompt();
+
 console.log('Please enter first operand');
 const op1 = readline.prompt();
 
 console.log('Please enter second operand');
 const op2 = readline.prompt();
 
-var answer = parseInt(op1) * parseInt(op2);
+var answer;
+switch (operator) {
+    case '+':
+        answer = parseInt(op1) + parseInt(op2);
+        break;
+    case '-':
+        answer = parseInt(op1) - parseInt(op2);
+        break;
+    case '*':
+        answer = parseInt(op1) * parseInt(op2);
+        break;
+    case '/':
+            answer = parseInt(op1) / parseInt(op2);
+            break;
+
+}
+
 console.log(answer);
