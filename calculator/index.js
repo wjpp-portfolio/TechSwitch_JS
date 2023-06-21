9a0f0ae642738e37a839159aac35429de3abed04
Will <will@wiri.co.uk>
2023-06-21 10:45:05 +0100

    refactored to include functions and code reuse

--- calculator/index.js
+++ calculator/index.js
@@ -1,33 +1,44 @@
 const readline = require('readline-sync');
-console.log('Welcome to the calculator!');
 
-console.log('Please enter desired operator');
-const operator = readline.prompt();
-
-console.log('Please enter operands separated by a space');
-var space_separated_operands = readline.prompt();
-
-space_separated_operands = space_separated_operands.replace(/\s/g, operator);
-
-console.log(eval(space_separated_operands));
-
-/*
-var answer;
-switch (operator) {
-    case '+':
-        answer = parseInt(op1) + parseInt(op2);
-        break;
-    case '-':
-        answer = parseInt(op1) - parseInt(op2);
-        break;
-    case '*':
-        answer = parseInt(op1) * parseInt(op2);
-        break;
-    case '/':
-            answer = parseInt(op1) / parseInt(op2);
-            break;
-
+function PrintMessage(msg) {
+    console.log(msg);
+}
+function CheckForExitSignal(passed_string){
+    if (passed_string.includes('q')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function ExitWithMessage(prompt) {
+    throw new Error(prompt);
+}
+function GatherInput(prompt) {
+    console.log(prompt);
+    const message = readline.prompt();
+    if (CheckForExitSignal(message)) {
+        ExitWithMessage('Exit signal q detected.');   
+    }
+    return message;
+}
+function ParseInputString(passed_string, operator) {
+    return passed_string.replace(/\s/g, operator);
+}
+function Calculate(expression) {
+    try {
+        var answer = eval(expression);
+    } catch (e) {
+        return 'Invalid input';
+    }
+    return answer;
 }
 
-console.log(answer);
-*/
+PrintMessage('Welcome to the calculator!');
+
+while (true){
+
+    const operator = GatherInput('Please enter desired operator. (Enter q to quit)');
+    const operands = GatherInput('Please enter space-separated integers');
+
+    PrintMessage(Calculate(ParseInputString(operands, operator))); 
+}
